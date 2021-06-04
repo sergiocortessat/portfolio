a6b3e3e47194ec9570402920d39e972afe5693c9
Sergio Cortes <sergiocortessat@gmail.com>
2021-06-04 15:25:36 +0200

    Add fixe bugs on active hover menu and highlight text

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// const smile = document.querySelector(".smile-hov")
+// const contactMe = document.querySelector(".contact-me-hov")
+
+// if(window.location.href === "http://127.0.0.1:5500/#testimonials") {
+//     console.log("yes")
+// }
+
+// console.log("           ")
+// console.log(smile.parentNode.previousSibling)
+// console.log(window.location.href)
+// console.log("           ")
