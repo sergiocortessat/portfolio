0fbbe2851b98ebf5ee0a5c9869024d5f251597b7
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-08 17:00:18 +0200

    Add changes to kwes io script

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,9 @@
 // console.log(smile.parentNode.previousSibling)
 // console.log(window.location.href)
 // console.log("           ")
+
+// let kwesScript = document.createElement('script')
+
+// kwesScript.setAttribute('src', 'https://kwes.io/js/kwes.js')
+
+// document.head.appendChild(kwesScript)
